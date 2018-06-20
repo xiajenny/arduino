4bab7e668ac8f6aa0372bb75c8ca9b27d73c8a40
Jenny Xia <33613456+xiajenny@users.noreply.github.com>
2018-06-20 18:24:49 -0400

    Add files via upload

---
+++ stopwatch.ino
@@ -0,0 +1,24 @@
+const int switchPin = 2; // the number of the input pin
+long startTime; // store starting time here
+long duration; // variable to store how long the timer has been running
+float secduration; // variable to store the duration in seconds
+
+
+void setup() {
+  pinMode(switchPin, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if( digitalRead(switchPin) == LOW ) {
+  startTime = millis();
+  }
+  
+  while ( digitalRead(switchPin) == LOW ) {
+  duration = millis() - startTime;
+  secduration = (float)duration / 1000;
+  Serial.print("Time: "); // print out your results
+  Serial.print(secduration);
+  Serial.println(" seconds");
+  }
+}
